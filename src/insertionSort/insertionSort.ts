9b855c74131fc69fc215b2c160b9e2967b4a6942
Zac Jones <zacjones93@gmail.com>
2017-07-12 15:56:46 -0700

    updated for plunker

--- src/insertionSort/insertionSort.ts
+++ src/insertionSort/insertionSort.ts
@@ -1,7 +1,7 @@
 /**
  * Sorts an array using insertion sort
  */
-export function insertionSort(array: number[]): number[] {
+function insertionSort(array: number[]): number[] {
   array = array.slice();
   for (let i = 1; i < array.length; i++) {
     const current = array[i];
@@ -14,3 +14,7 @@ export function insertionSort(array: number[]): number[] {
   }
   return array;
 }
+
+const unsortedArray = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+console.log(insertionSort(unsortedArray))
