4dc5fbf8704d4110cfdb1abead3d8b16b1271ba4
Zac Jones <zacjones93@gmail.com>
2017-07-12 17:00:16 -0700

    updated for plunker

--- src/repeatedItem/repeatedItem.ts
+++ src/repeatedItem/repeatedItem.ts
@@ -2,7 +2,7 @@
  * Returns the first repeated item from an array if any
  * @throws {Error} if there is no item repetition
  */
-export function repeatedItemLoops<T>(array: T[]): T {
+function repeatedItemLoops<T>(array: T[]): T {
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
       if (array[i] === array[j]) return array[i];
@@ -15,11 +15,19 @@ export function repeatedItemLoops<T>(array: T[]): T {
  * Returns the first repeated item from an array if any
  * @throws {Error} if there is no item repetition
  */
-export function repeatedItem<T>(array: T[]): T {
+function repeatedItem<T>(array: T[]): T {
   const set = new Set<T>();
   for (const item of array) {
     if (set.has(item)) return item;
     else set.add(item);
   }
-  throw new Error('No item repetition');
+  // throw new Error('No item repetition');
+  // converted for plunker output
+  return Error('No item repetition');
 }
+
+const itemRepetition = [1, 3, 5, 9, 1]
+const noItemRepetition = [1, 3, 5, 9]
+
+console.log(repeatedItem(itemRepetition))
+console.log(repeatedItem(noItemRepetition))
